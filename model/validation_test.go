745144dc1377f3c375ef928e2d8e52655a1dcb41
Narapong Hongthai <138201998+Phadoo131@users.noreply.github.com>
2023-10-19 11:08:05 +0700

    Update validation_test.go

--- model/validation_test.go
+++ model/validation_test.go
@@ -9,45 +9,45 @@ import (
 
 func TestFunOrFalse(t *testing.T) {
 	require := require.New(t)
-
-    // Test with int
-    result := test.FunOrFalse(5)
-    require.True(result)
-
-    // Test with rune
-    result = test.FunOrFalse(rune('5'))
-    require.True(result)
-
-    // Test with byte
-    result = test.FunOrFalse(byte('5'))
-    require.True(result)
-
-    // Test with string
-    result = test.FunOrFalse("Hello, World!")
-    require.False(result)
-
-    // Test with []string
-    result = test.FunOrFalse([]string{"hello", "5"})
-    require.True(result)
-
-    // Test with unsupported type
-    result = test.FunOrFalse(true)
-    require.False(result)
-
-    // Test with random byte
-    randByte := util.RandomByte()
-    result = test.FunOrFalse(randByte)
-    require.NotEmpty(t, result)
-
-    // Test with random int
-    randInt := util.RandomInt(12345, 123456)
-    result = test.FunOrFalse(randInt)
-    require.NotEmpty(t, result)
-
-    // Test with random string
-    randString := util.RandomString(7) + "!"
-    result = test.FunOrFalse(randString)
-    require.NotEmpty(t, result)
+	
+	 // Test with int
+	result := test.FunOrFalse(5)
+	require.True(result)
+	  
+	// Test with rune
+	result = test.FunOrFalse(rune('5'))
+	require.True(result)
+	
+	// Test with byte
+	result = test.FunOrFalse(byte('5'))
+	require.True(result)
+	
+	// Test with string
+	result = test.FunOrFalse("Hello, World!")
+	require.False(result)
+	
+	// Test with []string
+	result = test.FunOrFalse([]string{"hello", "5"})
+	require.True(result)
+	
+	// Test with unsupported type
+	result = test.FunOrFalse(true)
+	require.False(result)
+	
+	// Test with random byte
+	randByte := util.RandomByte()
+	result = test.FunOrFalse(randByte)
+	require.NotEmpty(t, result)
+	
+	    // Test with random int
+	randInt := util.RandomInt(12345, 123456)
+	result = test.FunOrFalse(randInt)
+	require.NotEmpty(t, result)
+	
+	// Test with random string
+	randString := util.RandomString(7) + "!"
+	result = test.FunOrFalse(randString)
+	require.NotEmpty(t, result)
 }
 
 func TestWhereIsFun(t *testing.T) {
